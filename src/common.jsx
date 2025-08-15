0ce7523bfb08848267bae51f6a46119e38fc9d51
abakm <akhilbalakrishnanakm@gmail.com>
2025-08-15 21:19:32 +0530

    Backend URL changed

--- src/common.jsx
+++ src/common.jsx
@@ -1,4 +1,4 @@
-var  backend_url = "https://shopgenie-api-production-07if.up.railway.app";
+var  backend_url = "https://shopgenie-api-production-071f.up.railway.app";
 // var backend_url = "http://localhost:5000";
 
 export default backend_url;
